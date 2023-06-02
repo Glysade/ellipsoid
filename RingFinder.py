fb2357d039d9a89800bf6c677e8fbec6375a3cc6
Iona Jones <ionapatriciajones@gmail.com>
2023-06-01 18:22:44 -0600

    Expanded rings, edited tests.

--- RingFinder.py
+++ RingFinder.py
@@ -5,7 +5,7 @@ from rdkit import Chem
 from rdkit.Chem.rdchem import Mol, Atom, Bond
 
 class RingFinder:
-    def __init__(self, mol):
+    def __init__(self, mol, addRingNieghbors=False):
         self.mol = mol 
         assigned_atoms = set()
         self.assigned_atoms = assigned_atoms
@@ -24,8 +24,9 @@ class RingFinder:
                 j = bond.GetIdx()
                 ring_bonds.add(j)
         self.ring_bonds = ring_bonds
+        self.rings = self._find_rings()
 
-    def ring_neighbors(self, atom_index):
+    def _ring_neighbors(self, atom_index):
         ring_neighbors = []
         if atom_index not in self.ring_atoms:
             return []
@@ -38,7 +39,7 @@ class RingFinder:
                 ring_neighbors.append(idx)
         return ring_neighbors
 
-    def find_next_ring(self):
+    def _find_next_ring(self):
         ring_atoms = self.ring_atoms
         assigned_atoms = self.assigned_atoms
         start = None
@@ -55,7 +56,7 @@ class RingFinder:
             grow_ring = False
             for i in range(len(current_ring)):
                 current_atom = current_ring[i]
-                neighbors = self.ring_neighbors(current_atom)
+                neighbors = self._ring_neighbors(current_atom)
                 for neighbor in neighbors:
                     if neighbor not in current_ring:
                         grow_ring = True
@@ -64,20 +65,31 @@ class RingFinder:
         current_ring.sort()
         return current_ring
 
-    def find_rings(self):
+    def _find_rings(self):
         all_rings = []
-        current_ring = self.find_next_ring()
+        current_ring = self._find_next_ring()
         while current_ring:
             all_rings.append(current_ring)
-            current_ring = self.find_next_ring();
+            current_ring = self._find_next_ring();
         return all_rings
+        self.all_rings = all_rings
+
+    def _find_complete_rings(self):
+        for ring in self.rings:
+            for atom_index in range(len(ring)):
+                atom = self.mol.GetAtomWithIdx(atom_index)
+                neighbors = atom.GetNeighbors()
+                for neighbor in neighbors:
+                    idx = neighbor.GetIdx()
+                    if idx not in self.assigned_atoms:
+                        ring.append(idx)
+                        self.assigned_atoms.add(idx)
+                    pass
 
 if __name__ == '__main__':
     smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
     m = Chem.MolFromSmiles(smiles)
     ringFinder = RingFinder(m)
-    neighbors = ringFinder.ring_neighbors(4)
-    # shoud be [5, 3]
-    rings = ringFinder.find_rings()
+    ringFinder._find_complete_rings()
     pass
 
--- test_RingFinder.py
+++ test_RingFinder.py
@@ -9,41 +9,34 @@ class RingFinderTest(unittest.TestCase):
         smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
         mol = Chem.MolFromSmiles(smiles)
         ringFinder = RingFinder(mol)
-        neighbors = ringFinder.ring_neighbors(4)
+        neighbors = ringFinder._ring_neighbors(4)
         self.assertCountEqual(neighbors, [3,5])
 
     def test_neighbors2(self):
         smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
         mol = Chem.MolFromSmiles(smiles)
         ringFinder = RingFinder(mol)
-        neighbors = ringFinder.ring_neighbors(12)
+        neighbors = ringFinder._ring_neighbors(12)
         self.assertCountEqual(neighbors, [11,13])
     
     def test_neighbors3(self):
         smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
         mol = Chem.MolFromSmiles(smiles)
         ringFinder = RingFinder(mol)
-        neighbors = ringFinder.ring_neighbors(15)
+        neighbors = ringFinder._ring_neighbors(15)
         self.assertCountEqual(neighbors, [16,24])
     
     def test_neighbors4(self):
         smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
         mol = Chem.MolFromSmiles(smiles)
         ringFinder = RingFinder(mol)
-        neighbors = ringFinder.ring_neighbors(23)
+        neighbors = ringFinder._ring_neighbors(23)
         self.assertCountEqual(neighbors, [18,24,22])
-
-    def test_ring_finder(self):
-        smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
-        mol = Chem.MolFromSmiles(smiles)
-        ringFinder = RingFinder(mol)
-        ring = ringFinder.find_next_ring()
-        self.assertCountEqual(ring, [1,2,3,4,5])
 
     def test_rings(self):
         smiles = 'Cc1c(cc([nH]1)C(=O)NC2CCN(CC2)c3ccc4ccccc4n3)Br'
         mol = Chem.MolFromSmiles(smiles)
         ringFinder = RingFinder(mol)
-        rings = ringFinder.find_rings()
+        rings = ringFinder.rings
         self.assertCountEqual(rings, [[1,2,3,4,5], [9,10,11,12,13,14],[15,16,17,18,19,20,21,22,23,24]])
 
